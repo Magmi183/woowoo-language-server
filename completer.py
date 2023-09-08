b9981d8d34397a52acf45fe89f388d1d47d87fb1
Michal Janecek <michal.janecek@gmail.com>
2023-09-08 15:18:44 +0200

    Add autocompletion for the include statement + start README (features section).

--- completer.py
+++ completer.py
@@ -1,4 +1,10 @@
-from lsprotocol.types import CompletionParams, CompletionList, CompletionItem
+import os
+
+from lsprotocol.types import CompletionParams, CompletionList, CompletionItem, CompletionItemKind, InsertTextFormat
+
+import tree_utils
+import utils
+from woowoodocument import WooWooDocument
 
 
 class Completer:
@@ -14,6 +20,31 @@ class Completer:
 
     def _complete(self, params: CompletionParams) -> [CompletionItem]:
         # https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#completionItem
-        # TODO: Do the actual work.
+        # TODO: Add more completion options.
+        document = self.ls.get_document(params)
+        completion_items = []
 
-        return [CompletionItem(label="bold"), CompletionItem(label="italic")]
+        completion_items += self.complete_include(document, params)
+
+        return completion_items
+
+
+    def complete_include(self, document: WooWooDocument, params: CompletionParams):
+
+        # TODO: Do more conditions. This is POC.
+        if tree_utils.is_query_overlapping_pos(document.tree, "(text_block) @tb", params.position.line, params.position.character):
+            return []
+
+        current_doc_path = utils.uri_to_path(params.text_document.uri)
+
+        # relative paths of woo files in the workspace
+        relative_paths = [os.path.relpath(path, os.path.dirname(current_doc_path)) for path in self.ls.docs.keys()]
+        paths_joined = ",".join(relative_paths)
+
+        return [CompletionItem(
+                    label=".include",
+                    kind=CompletionItemKind.Snippet,
+                    insert_text_format=InsertTextFormat.Snippet,
+                    insert_text=f'include ${{1|{paths_joined}|}}'
+                )]
+
--- tree_utils.py
+++ tree_utils.py
@@ -1,6 +1,14 @@
+from parser import WOOWOO_LANGUAGE
+
 
 def build_query_string_from_list(node_type_list, capture_name):
     formatted_nodes = [f"({node})" for node in node_type_list]
     query_string = f"[ {' '.join(formatted_nodes)} ] @{capture_name}"
 
     return query_string
+
+
+def is_query_overlapping_pos(tree, query, line, col, lang = WOOWOO_LANGUAGE) -> bool:
+    nodes = lang.query(query).captures(tree.root_node, start_point=(line, col), end_point=(line, col+1))
+
+    return len(nodes) > 0
