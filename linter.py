ea43d524c674126d66eb0a319239e10283495811
Michal Janecek <michal.janecek@gmail.com>
2023-10-07 17:17:42 +0200

    Add missing node diagnosis

--- linter.py
+++ linter.py
@@ -1,5 +1,6 @@
 from lsprotocol.types import Diagnostic, Range, Position, DiagnosticSeverity
 from parser import WOOWOO_LANGUAGE
+from tree_sitter import Node
 
 
 class Linter:
@@ -13,9 +14,39 @@ class Linter:
 
         # TODO: Add other types of linting.
         diagnostics += self.diagnose_errors(tree)
+        
+        diagnostics += self.diagnose_missing_nodes(tree)
 
         # send a notification with the diagnostics to the client
         self.ls.publish_diagnostics(params.text_document.uri, diagnostics)
+
+    def diagnose_missing_nodes(self, tree) -> [Diagnostic]:
+        """
+        Currently it is not possible to query MISSING nodes, see open issue: https://github.com/tree-sitter/tree-sitter/issues/606
+        Therefore, a workaround has to be used.
+        """
+        diagnostics = []
+        missing_nodes = []
+
+        def traverse_tree(node: Node):
+            for n in node.children:
+                if n.is_missing:
+                    missing_nodes.append(n)
+                traverse_tree(n)
+
+        traverse_tree(tree.root_node)
+        for missing_node in missing_nodes:
+            diagnostics.append(Diagnostic(
+                range=Range(start=Position(*missing_node.start_point),
+                            end=Position(missing_node.end_point[0], missing_node.end_point[1]+1)),
+                message=f"Syntax error: MISSING {missing_node.type}",
+                source=self.ls.name,
+                severity=DiagnosticSeverity.Error
+            ))
+        
+        # TODO: Add automatic fixes?
+
+        return diagnostics
 
     def diagnose_errors(self, tree) -> [Diagnostic]:
         # TODO: Review and test this function.
