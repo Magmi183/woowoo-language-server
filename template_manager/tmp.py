055d641961584153f92703dc2408fdd9ae0fd8d0
Michal Janecek <michal.janecek@gmail.com>
2024-01-16 10:31:20 +0100

    cleanup

---
+++ template_manager/tmp.py
@@ -0,0 +1,11 @@
+from template_manager import TemplateManager
+from pprint import pprint
+
+# Test script
+
+yaml_path = '/Users/michaljanecek/Diplomka/woowoo-pygls/template_sandbox/fit_math.yaml'
+
+template_manager = TemplateManager(yaml_path)
+
+template = template_manager.active_template
+
