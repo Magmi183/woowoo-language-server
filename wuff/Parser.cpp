071e9b5a749fb89d2a46a202b027583377d0e148
Michal Janecek <michal.janecek@gmail.com>
2024-01-28 11:13:17 +0100

    add treesitter dependency

---
+++ wuff/Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michal Janecek on 28.01.2024.
+//
+
+#include "Parser.h"
---
+++ wuff/Parser.h
@@ -0,0 +1,16 @@
+//
+// Created by Michal Janecek on 28.01.2024.
+//
+
+#ifndef WUFF_PARSER_H
+#define WUFF_PARSER_H
+
+#include <tree_sitter/api.h>
+
+
+class Parser {
+
+};
+
+
+#endif //WUFF_PARSER_H
